f92606e0735bbf83dd2824f40233cfe1d418e55c
Bruno Cardoso <105465245+brun0car1oso@users.noreply.github.com>
2025-04-26 12:15:27 +0000

    Add files via upload
    Implementação script

---
+++ script-iac-servidor-web.sh
@@ -0,0 +1,23 @@
+echo -e "Atualizando servidor\n"
+
+apt-get update && apt-get upgrade 
+
+echo -e "Instalando o servidor apache2\n"
+sudo apt-get install apache2 -y 
+
+echo -e "Instalando o unzip\n"
+sudo apt-get install unzip -y 
+
+cd /tmp
+
+if [ wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip ] then
+     echo -e "Baixando aplicação\n"
+
+else
+     echo -e "instalando wget\n" 
+     sudo apt-get install wget -y 
+fi
+
+echo -e "Copiando arquivos para diretório padrão do apache"
+
+cp -vr archive /var/www/html 
