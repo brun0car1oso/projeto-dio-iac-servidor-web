9810b356712de5e8c7dd504cfbce10bc32b5b532
brun0 car1oso <brunnodmais@gmail.com>
2025-04-27 17:26:25 -0300

    stable version script

--- script-iac-servidor-web.sh
+++ script-iac-servidor-web.sh
@@ -1,17 +1,17 @@
 echo -e "Atualizando servidor \n"
-apt-get update && apt-get upgrade 
+apt-get update && apt-get upgrade
 
 sleep 5
 clear
 
 echo -e "Instalando o servidor apache2 \n"
-apt-get install apache2 -y 
+apt-get install apache2 -y
 
 sleep 5
 clear
 
 echo -e "Instalando o unzip \n"
-apt-get install unzip -y 
+apt-get install unzip -y
 
 sleep 5
 clear
@@ -22,16 +22,16 @@ cd /tmp
 echo -e "Baixando aplicação\n"
 
 if wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip; then
-     
+
      echo -e "Aplicação baixada \n"
 
 else
-     echo -e "instalando wget \n" 
+     echo -e "instalando wget \n"
      apt-get install wget -y
 
      echo -e "Baixando aplicação \n"
      wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-     
+
      echo -e "Aplicação baixada \n"
 
 fi
@@ -39,9 +39,9 @@ fi
 echo -e "Descompactando arquivos \n"
 unzip main.zip
 
-cd linux-site-dio
+cd linux-site-dio-main
 
 echo -e "Copiando arquivos para diretório padrão do apache \n"
-cp -rR * /var/www/html 
+cp -rR * /var/www/html
 
 echo "FIM"
